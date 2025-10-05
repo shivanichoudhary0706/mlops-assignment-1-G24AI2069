386ab50b6c2cc3ee43aaaf9f5702871661f20321
shivanichoudhary0706 <shivanichaudhary0706@gmail.com>
2025-10-05 12:00:43 +0530

    Removed 3 files

--- train.py
+++
@@ -1,9 +0,0 @@
-from sklearn.tree import DecisionTreeRegressor
-from misc import load_data, preprocess, train_and_evaluate
-
-df = load_data()
-X_train, X_test, y_train, y_test = preprocess(df)
-
-model = DecisionTreeRegressor(random_state=42)
-mse = train_and_evaluate(model, X_train, X_test, y_train, y_test)
-print(f'DecisionTreeRegressor Test MSE: {mse:.4f}')
